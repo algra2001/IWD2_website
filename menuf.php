22b2a5a7c6d439f3d6adb73496fa7585a033c4da
algra2001 <dandush1001@gmail.com>
2026-02-21 17:47:39 +0000

    Starter main page (index) and menu php scripts

---
+++ menuf.php
@@ -0,0 +1,26 @@
+<?php // Setting menu options
+echo <<<_MENU1
+   Your options are <br>
+    <table width ="70%" border="0" cellspacing="0" align="center"> <tr>
+   <td bgcolor="#DCEFFE"><div align="center">
+<!-- query submit form -->
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2883992/DL/p1.php"> Submit a query </a>
+    </div></td>
+   <td bgcolor="#DCEFFE"><div align="center">
+
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2883992/DL/p2.php"> Search Compounds </a>
+    </div></td>
+   <td bgcolor="#DCEFFE"><div align="center">
+   <td bgcolor="#DCEFFE"><div align="center">
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2883992/DL/p3.php"> Stats </a>
+    </div></td>
+   <td bgcolor="#DCEFFE"><div align="center">
+   <td bgcolor="#DCEFFE"><div align="center">
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2883992/DL/p4.php"> Correlations </a>
+    </div></td>
+   <td bgcolor="#DCEFFE"><div align="center">
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2883992/DL/p5.php"> Exit </a>
+    </div></td>
+    </tr></table>
+_MENU1;
+?>
