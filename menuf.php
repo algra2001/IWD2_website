18f2fd66172af7c411cc70260783e8e5708b7d35
algra2001 <dandush1001@gmail.com>
2026-02-21 17:47:39 +0000

    Initial menu setup

--- menuf.php
+++ menuf.php
@@ -1,25 +1,48 @@
-<?php // Setting menu options
+<?php // Setting menu options, adapted from class code
 echo <<<_MENU1
    Your options are <br>
     <table width ="70%" border="0" cellspacing="0" align="center"> <tr>
    <td bgcolor="#DCEFFE"><div align="center">
+
 <!-- query submit form -->
-    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2883992/DL/p1.php"> Submit a query </a>
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2883992/website/query.php"> Submit a query </a>
     </div></td>
    <td bgcolor="#DCEFFE"><div align="center">
 
-    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2883992/DL/p2.php"> Search Compounds </a>
+<!-- example dataset analysis -->
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2883992/website/example.php"> Example dataset </a>
+    </div></td>
+
+   <td bgcolor="#DCEFFE"><div align="center">
+   <td bgcolor="#DCEFFE"><div align="center">
+
+<!-- previous results page -->
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2883992/website/previous_results.php"> Previous results </a>
     </div></td>
    <td bgcolor="#DCEFFE"><div align="center">
    <td bgcolor="#DCEFFE"><div align="center">
-    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2883992/DL/p3.php"> Stats </a>
+
+<!-- help page -->
+   <td bgcolor="#DCEFFE"><div align="center">
+   <td bgcolor="#DCEFFE"><div align="center">
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2883992/website/help_page.php"> Help </a>
+    </div></td>
+
+<!--  about page -->
+   <td bgcolor="#DCEFFE"><div align="center">
+   <td bgcolor="#DCEFFE"><div align="center">
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2883992/website/about.php"> About </a>
+    </div></td>
+
+<!-- statment of credit page -->
+   <td bgcolor="#DCEFFE"><div align="center">
+   <td bgcolor="#DCEFFE"><div align="center">
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2883992/website/credit.php"> Statement of credit </a>
     </div></td>
    <td bgcolor="#DCEFFE"><div align="center">
-   <td bgcolor="#DCEFFE"><div align="center">
-    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2883992/DL/p4.php"> Correlations </a>
-    </div></td>
-   <td bgcolor="#DCEFFE"><div align="center">
-    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2883992/DL/p5.php"> Exit </a>
+
+<!-- exit page -->
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2883992/website/exit.php"> Exit </a>
     </div></td>
     </tr></table>
 _MENU1;
